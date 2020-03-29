792c8a04fa8472f6b9449c7fc10c67f31e659295
Elisha Lye <elisha.lye@rea-group.com>
2020-03-29 14:36:30 +1100

    Upgrade to Android X

--- app/src/main/java/com/elyeproj/surfaceviewexplore/MainActivity.kt
+++ app/src/main/java/com/elyeproj/surfaceviewexplore/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.elyeproj.surfaceviewexplore
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.slow_surfaceview_animation
 import kotlinx.android.synthetic.main.activity_main.slow_view_animation
--- app/src/main/java/com/elyeproj/surfaceviewexplore/SlowViewActivity.kt
+++ app/src/main/java/com/elyeproj/surfaceviewexplore/SlowViewActivity.kt
@@ -1,6 +1,6 @@
 package com.elyeproj.surfaceviewexplore
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SlowViewActivity : AppCompatActivity() {
